b1d505b52fe6b3c11422d7126f8f456548489a2c
Diogo Julio <diogosarti13@gmail.com>
2024-09-21 21:02:50 -0300

    cors aberto

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    cors: { origin: [process.env.WEB_URL, 'http://127.0.0.1:5173'] },
+    cors: { origin: [process.env.WEB_URL, '*'] },
   });
   await app.listen(3000);
 }
