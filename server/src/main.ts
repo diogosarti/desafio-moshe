510ad49365d77c0bb48b9a092c1d0d05702ccbd8
Diogo Julio <diogosarti13@gmail.com>
2024-09-21 21:23:29 -0300

    cors configurado

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,9 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: { origin: [process.env.WEB_URL, '*'] },
+  const app = await NestFactory.create(AppModule);
+  const allowedOrigins = [process.env.WEB_URL];
+  app.enableCors({
+    origin: allowedOrigins,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
   });
+
   await app.listen(3000);
 }
 bootstrap();
